06eb51aeb9f2362dfa0b5b76846faa618d2e571f
STEP9N <smartymartyn@yandex.ru>
2022-06-26 20:20:08 +0500

    practice 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 25.06.22
     19:45 started
     21:04 practice 1
+26.06.22
+    20:18 practice 2
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-// let personalMovieDB = {
+// const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
+// const personalMovieDB = {
 //     count: numberOfFilms,
 //     movies: {},
 //     actors: {},
@@ -9,16 +9,41 @@
 //     privat: false
 // };
 
-// let lastFilm = prompt('Один из последних просмотренных фильмов?');
-// let lastFilmRating = prompt('На сколько оцените его?');
+// const a = prompt('Один из последних просмотренных фильмов?'),
+//     b = +prompt('На сколько оцените его?'),
+//     c = prompt('Один из последних просмотренных фильмов?'),
+//     d = +prompt('На сколько оцените его?');
 
-// personalMovieDB.movies = {
-//     'lastFilm': 'lastFilmRating'
-// };
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 // console.log(personalMovieDB.movies);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// let a = prompt('Один из последних просмотренных фильмов?');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB.movies);
+
+// let a = prompt('Один из последних просмотренных фильмов?');
+
+// if (a == null || a.length == 0 || a.length >= 50) {
+//     a = prompt('Введите один из последних просмотренных фильмов, пожалуйста');
+// };
+
+
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -27,12 +52,23 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-    b = +prompt('На сколько оцените его?'),
-    c = prompt('Один из последних просмотренных фильмов?'),
-    d = +prompt('На сколько оцените его?');
+for (let i = 1; i < 5; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?')
+    while (a == null || a.length == 0 || a.length >= 50) {
+        a = prompt('Введите один из последних просмотренных фильмов, пожалуйста.')
+    }
+    let b = prompt('Насколько оцените его?')
+    personalMovieDB.movies[a] = b
+};
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (numberOfFilms <= 10 && numberOfFilms >= 0) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms > 10 && numberOfFilms < 30) {
+    alert('Вы классический зритель');
+} else if (numberOfFilms >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Ошибка');
+};
 
 console.log(personalMovieDB.movies);
